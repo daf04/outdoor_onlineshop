12b3466e5a129e34f2c6c40a1d496f4de88b02e1
daf04 <aldrichdaffa0401@gmail.com>
2022-02-05 15:18:17 +0700

    finish

---
+++ detail-produk.php
@@ -0,0 +1,82 @@
+<?php
+error_reporting(0);
+include 'db.php';
+
+$kontak = mysqli_query($conn, "SELECT admin_telp, admin_email, admin_address FROM tb_admin WHERE admin_id = 1");
+$a = mysqli_fetch_object($kontak);
+
+$produk = mysqli_query($conn, "SELECT * FROM tb_product WHERE product_id = '" . $_GET['id'] . "'");
+$p = mysqli_fetch_object($produk);
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width-device-width, initial-scale=1">
+  <title>TOREAN OUTDOOR STORE</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+  <!-- Header -->
+  <header>
+    <div class="container">
+      <h1><a href="index.php">Torean Outdoor</a></h1>
+      <ul>
+        <li><a href="produk.php">Produk</a></li>
+      </ul>
+    </div>
+  </header>
+  <!-- Search -->
+  <div class="search">
+    <div class="container">
+      <form action="produk.php">
+        <input type="text" name="search" placeholder="Cari Produk" value="<?php echo $_GET['search'] ?>">
+        <input type="hidden" name="kat" value="<?php echo $_GET['kat'] ?>">
+        <input type="submit" name="cari" value="Cari Produk">
+      </form>
+    </div>
+  </div>
+
+  <!-- Produk detial -->
+  <div class="section">
+    <div class="container">
+      <h3>Detail Produk</h3>
+      <div class="box">
+        <div class="col-2">
+          <img src="produk/<?php echo $p->product_image ?>" width="80%" border="5px solid #333">
+        </div>
+        <div class="col-2">
+          <h3><?php echo $p->product_name ?></h3>
+          <h4>Rp. <?php echo number_format($p->product_price) ?></h4>
+          <p>Deskripsi :<br>
+            <?php echo $p->product_description ?>
+          </p>
+          <p><a href="https://api.whatsapp.com/send?phone=<?php echo $a->admin_telp ?>&text=Hai, Saya tertarik dengan produk anda." target="_blank">
+              Hubungi via Whatsapp
+              <img src="img/whatsapp.png" width="50px" </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Footer -->
+  <div class="footer">
+    <div class="container">
+      <h4>Alamat</h4>
+      <p><?php echo $a->admin_address ?></p>
+
+      <h4>Email</h4>
+      <p><?php echo $a->admin_email ?></p>
+
+      <h4>No. Hp</h4>
+      <p><?php echo $a->admin_telp ?></p>
+      <small>Copyright &copy; 2022 - Torean Outdoor Store.</small>
+    </div>
+  </div>
+
+</body>
+
+</html>
--- produk.php
+++ produk.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 include 'db.php';
 
 $kontak = mysqli_query($conn, "SELECT admin_telp, admin_email, admin_address FROM tb_admin WHERE admin_id = 1");
@@ -28,43 +29,23 @@ $a = mysqli_fetch_object($kontak);
   <div class="search">
     <div class="container">
       <form action="produk.php">
-        <input type="text" name="search" placeholder="Cari Produk">
+        <input type="text" name="search" placeholder="Cari Produk" value="<?php echo $_GET['search'] ?>">
+        <input type="hidden" name="kat" value="<?php echo $_GET['kat'] ?>">
         <input type="submit" name="cari" value="Cari Produk">
       </form>
-    </div>
-  </div>
-  <!-- category -->
-  <div class="section">
-    <div class="container">
-      <h3>Kategori</h3>
-      <div class="box">
-        <?php
-        $kategori = mysqli_query($conn, "SELECT * FROM tb_category ORDER BY category_id DESC");
-        if (mysqli_num_rows($kategori) > 0) {
-          while ($k = mysqli_fetch_array($kategori)) {
-
-        ?>
-            <a href="produk.php?kat=<?php echo $k['category_id'] ?>">
-              <div class="col-5">
-                <img src="img/eiger.jpg" width="50px" style="margin-bottom: 5px;">
-                <p><?php echo $k['category_name'] ?></p>
-              </div>
-            </a>
-          <?php }
-        } else { ?>
-          <p>Kategori tidak ada</p>
-        <?php } ?>
-      </div>
     </div>
   </div>
 
   <!-- new product -->
   <div class="section">
     <div class="container">
-      <h3>Produk Terbaru</h3>
+      <h3>Produk</h3>
       <div class="box">
         <?php
-        $produk = mysqli_query($conn, "SELECT * FROM tb_product WHERE product_status = 1 ORDER BY product_id DESC LIMIT 8");
+        if ($_GET['search'] != '' || $_GET['kat'] != '') {
+          $where = "AND product_name LIKE'%" . $_GET['search'] . "%' AND category_id LIKE'%" . $_GET['kat'] . "%'";
+        }
+        $produk = mysqli_query($conn, "SELECT * FROM tb_product WHERE product_status =1 $where ORDER BY product_id DESC");
         if (mysqli_num_rows($produk) > 0) {
           while ($p = mysqli_fetch_array($produk)) {
         ?>
